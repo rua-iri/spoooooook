207437420b8460d30ca7f10f3409f598a27cbc90
rua-iri <rory.mcguigan@protonmail.com>
2023-02-17 06:44:56 +0000

    implement video streaming

--- src/app/components/video/video.component.ts
+++ src/app/components/video/video.component.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { VideoUrl } from 'src/app/videourl';
 
 @Component({
   selector: 'app-video',
@@ -10,17 +12,20 @@ import { ActivatedRoute } from '@angular/router';
 export class VideoComponent implements OnInit{
   
   public filmId: string;
-  videoUrl: string;
+  videoUrl: Observable<VideoUrl[]>;
+  readonly baseUrl: string = "https://zkxb1yonjc.execute-api.us-east-1.amazonaws.com/FilmDetails/video?imdbID=";
   
+
   constructor(private route: ActivatedRoute, private http: HttpClient) { }
 
   ngOnInit(): void {
     this.filmId = this.route.snapshot.paramMap.getAll("videoId")[0];
+    this.getVideo();
   }
 
   // function to get the url for the film 
   getVideo() {
-    
+    this.videoUrl = this.http.get<VideoUrl[]>(this.baseUrl + this.filmId);
   }
 
 }
