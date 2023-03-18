3eedddf64d3b7b1080aa88659826d6d6a2543d56
rua-iri <rory.mcguigan@protonmail.com>
2023-03-18 20:32:17 +0000

    update form to support submission functionality

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,6 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+
+
+  onSubmit(loginForm: NgForm) {
+    console.log(loginForm.value);
+    console.log(loginForm.value.username);
+    console.log(loginForm.value.password);
+    // TODO submit form to backend to check if user exists and details are correct
+
+  }
 
 
 }
