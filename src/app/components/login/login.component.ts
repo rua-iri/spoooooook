6f09690b201ed272ef19387e410f417b936a942d
rua-iri <rory.mcguigan@protonmail.com>
2023-03-19 23:43:52 +0000

    connect aws login backend to website frontend

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormGroup, NgForm } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
@@ -8,14 +9,25 @@ import { FormGroup, NgForm } from '@angular/forms';
 })
 export class LoginComponent {
 
+  readonly baseUrl: string = "https://3z4kluwtp5.execute-api.us-east-1.amazonaws.com/real/login";
+
+  constructor(private http: HttpClient) { }
 
   onSubmit(loginForm: NgForm) {
-    console.log(loginForm.value);
+    // console.log(loginForm.value);
     console.log(loginForm.value.username);
     console.log(loginForm.value.password);
-    // TODO submit form to backend to check if user exists and details are correct
+
+    this.http.post(this.baseUrl, null, { "headers": { "username": loginForm.value.username, "password": loginForm.value.password } })
+      .subscribe((result) => {
+        console.warn(result);
+        if (result.loginResult==true) {
+          // TODO login user and store something in session storage to keep them logged in
+        }
+      })
 
   }
 
 
 }
+
