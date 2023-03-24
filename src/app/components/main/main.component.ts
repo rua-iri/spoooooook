4c1fd9f390e13edab8fc0c42b525eb6a1d770214
rua-iri <rory.mcguigan@protonmail.com>
2023-03-24 01:39:31 +0000

    style navbar and prompt users to register before using service

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-main',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./main.component.css']
 })
 export class MainComponent {
+
+  isLoggedIn: boolean;
+
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+    this.isLoggedIn = JSON.parse(sessionStorage.getItem("loggedIn")!);
+
+    // redirect to registration page if not logged in
+    if (!this.isLoggedIn) {
+      this.router.navigate(["register"]);
+    }
+  }
 
 }
